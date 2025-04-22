7ba25965b0842c7c298e160b9524abb04da26152
Jialinvip <129817260+Jialinvip@users.noreply.github.com>
2025-04-22 16:48:37 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 Deno.serve(() => {
-  return fetch("请访问: www.gptvip.cn");
+  return new Response("请访问: www.gptvip.cn", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+    },
+  });
 });
